d867ece855f6e75ea5255ec0e38e5ee6eaa9bd1a
qiuqiu-ji <jackqiuji@gmail.com>
2024-12-15 00:24:47 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -102,4 +102,6 @@ async def get_status(video_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080) 
+    import os
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port) 
